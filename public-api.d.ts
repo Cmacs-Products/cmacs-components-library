39c7d1bb1a49a45c98a94dff52e1ba4a6f1fa97d
raj.shetty <raj.shetty@sidhiconsulting.com>
2019-10-06 08:51:26 -0400

    * Component Moveable list: hotfixes * Merge pull request #78 from Cmacs-Products/feature/cmacs-lib * Component Gris: demo custom grid with pop up * Component Moveable list: interaction Modal * Component Moveable List: Basic Demo * Component Moveable: List * Fixed todo card

--- public-api.d.ts
+++ public-api.d.ts
@@ -95,3 +95,4 @@ export * from './lib/components/cmacs-tabs/cmacs-tabs-ink-bar.directive';
 export * from './lib/components/cmacs-tabs/cmacs-tabs-nav.component';
 export * from './lib/components/cmacs-tabs/cmacs-tabset.component';
 export * from './lib/components/cmacs-side-panel/cmacs-side-panel.component';
+export * from './lib/components/cmacs-moveable-list/cmacs-moveable-list.component';
