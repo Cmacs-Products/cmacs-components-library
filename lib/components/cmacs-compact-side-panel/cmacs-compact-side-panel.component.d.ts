8df1ac69707933fcfa68406e3b2bf32a3e6ad8f1
Prithvi Gowda <prithvi.gowda@sidhiconsulting.com>
2020-01-13 14:28:26 -0500

    Lib Updated

---
+++ lib/components/cmacs-compact-side-panel/cmacs-compact-side-panel.component.d.ts
@@ -0,0 +1,5 @@
+import { OnInit } from '@angular/core';
+export declare class CmacsCompactSidePanelComponent implements OnInit {
+    constructor();
+    ngOnInit(): void;
+}
--- public-api.d.ts
+++ public-api.d.ts
@@ -119,3 +119,4 @@ export * from './lib/components/cmacs-tree-select/cmacs-tree-select.component';
 export * from './lib/components/cmacs-tree-select/cmacs-tree-select.service';
 export * from './lib/components/cmacs-timeline-datepicker/cmacs-timeline-datepicker.component';
 export * from './lib/components/cmacs-xlsx-loader/cmacs-xlsx-loader.component';
+export * from './lib/components/cmacs-compact-side-panel/cmacs-compact-side-panel.component';
